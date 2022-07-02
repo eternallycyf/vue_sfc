fab2deea7c8da641a1106d8a13389d77bae8fea4
eternallycyf <a969475322@gmail.com>
2022-07-02 14:37:42 +0800

    feat:init webpack and parsing sfc

---
+++ modules/vue-loader/index.js
@@ -0,0 +1,15 @@
+const refTemplate = /\<template\>(.+?)\<\/template\>/;
+const regScript = /\<script\>(.+?)\<\/script\>/;
+const refFirstSign = /({)/;
+
+module.exports = function (source) {
+  const _source = source.replace(/[\r\n]/g, '');
+  const template = _source.match(refTemplate)[1];
+  const script = _source.match(regScript)[1];
+
+  const finalScript = script.replace(
+    refFirstSign,
+    '$1 template:' + '`' + template + '`' + ',',
+  );
+  console.log(finalScript);
+};
